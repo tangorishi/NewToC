35424362f9ac6f524d3fd39f1cfd7d820d2e9ff1
Rishi Joshi <110519406+tangorishi@users.noreply.github.com>
2023-01-24 02:39:10 +0530

    Add files via upload

---
+++ pallindrome.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main()
+{
+    int num,rem;
+    int result =0;
+
+    printf("enter the number you want to check for if it's a pallindrome or not\n");
+    scanf("%d" ,&num);
+
+ 
+    while(num!=0){
+
+         rem = num%10;
+         result = result*10+rem;
+        num = num/10;
+
+    }
+    if(result == num )
+    {
+        printf("%d is a pallindrome number" ,num);
+
+    }
+    else
+    printf("%d is not a pallindrome number" ,num);
+
+
+return 0;
+}
+void main()
+{   
+
+    __msvcrt_printf(___int_size_t_h)
+    while (q/_QUERY_MSVC_PRINTF_QUIRKS{
+        scanf(enter the elememts that you wnt to enter tmpfile)
+        printf(
+            "h;
+            
+        )
+    })
+
+}
